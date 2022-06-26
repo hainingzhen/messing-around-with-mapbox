fd63a9ee69fdefd36fea582e6cc5799b426a72b2
Haining Zhen <hainingzhen@outlook.com>
2022-06-26 22:09:04 +0100

    initial mapbox gl testing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useRef, useEffect, useState } from 'react';
+import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
+
+
 
 function App() {
+
+  mapboxgl.accessToken = "pk.eyJ1IjoiaGh6aGVuIiwiYSI6ImNsNHZidjFsejB3dHAzZHBoMTdlbHplM2kifQ.h1LjuVXTUf1y1SlzeRJezg";
+
+  console.log(mapboxgl.accessToken);
+
+  const mapContainer = useRef(null);
+  const map = useRef(null);
+  const [lng, setLng] = useState(-70.9);
+  const [lat, setLat] = useState(42.35);
+  const [zoom, setZoom] = useState(9);
+  
+  console.log(mapContainer.current)
+
+  useEffect(() => {
+      if (map.current) return; // initialize map only once
+      map.current = new mapboxgl.Map({
+          container: mapContainer.current,
+          style: 'mapbox://styles/mapbox/streets-v11',
+          center: [lng, lat],
+          zoom: zoom
+      });
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        MapBox
+        <div ref={mapContainer} className="map-container" />
     </div>
   );
 }
